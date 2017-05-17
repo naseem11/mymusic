30d75c75c951af2e0e7f563c0f77017ec11765a7
naseem11 <miannaseem11@gmail.com>
2017-05-17 13:59:05 +0100

    Added about page

--- mymusic/urls.py
+++ mymusic/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from music import urls as music_urls
 # from django.views.static import serve
 # from .settings import MEDIA_ROOT
-from .views import index
+from .views import index ,about
 from accounts import urls as accounts_urls
 
 
@@ -30,6 +30,7 @@ urlpatterns = [
     url(r'^accounts/', include((accounts_urls))),
     # url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     # url(r'^user/', include(accounts_urls)),
+    url(r'^about/$',about, name='about'),
     url(r'', index, name='index' ),
 
 ]
--- mymusic/views.py
+++ mymusic/views.py
@@ -6,3 +6,7 @@ def  index(request):
        return redirect('home')
     else:
         return render(request,'index.html')
+
+
+def about(request):
+    return render(request,'about.html')
